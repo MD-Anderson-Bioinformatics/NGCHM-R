a16fd76cefed9e1ef837865dc239167cad1e5ebf
Bradley Broom <bmbroom@mdanderson.org>
2015-08-27 10:42:39 -0500

    Don't include deploy server config if the latter is null.

--- inst/base.config/conf.d/01-server-protocol-manager.R
+++ inst/base.config/conf.d/01-server-protocol-manager.R
@@ -8,6 +8,12 @@ requireNamespace('httr')
   validator <- function (params) {
   };
 
+  configAndProgress <- function (server) {
+    cfg <- chmGetDeployServerConfig (server);
+    prg <- httr::progress("up");
+    if (is.null(cfg)) prg else c(cfg,prg)
+  }
+
 ngchmCreateServerProtocol ("manager",
     requiredParams = c('serviceName'),
     optionalParams = NULL,
@@ -18,7 +24,7 @@ ngchmCreateServerProtocol ("manager",
 	serviceName <- ngchmGetProtoParam (server, 'serviceName');
 	res <- httr::POST(url = sprintf ("%s/put", server@deployServer),
 	            body = list (server=serviceName, name=chm@name, limitAction='none', file=httr::upload_file (chmFileName)),
-	            encode = "multipart", c(chmGetDeployServerConfig(server),httr::progress("up")), handle=ngchmGetHandleHTTR(server));
+	            encode = "multipart", configAndProgress(server), handle=ngchmGetHandleHTTR(server));
 	cat("\n");
 	if ((res$status_code < 200) || (res$status_code >= 300)) {
 		cat(rawToChar(res$content), "\n")
