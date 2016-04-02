65e4ba2f34bc9ea035fae942e2431518d282e386
Bradley Broom <bmbroom@mdanderson.org>
2016-04-01 19:35:03 -0500

    Get dyce.js from the chm server, not bioinformatics.mdanderson.org

--- inst/extdata/custom.js
+++ inst/extdata/custom.js
@@ -1,21 +1,14 @@
 (function(chm){
-function _chm_as(src){var s=document.createElement('script');
- s.setAttribute('type','text/javascript'); s.setAttribute('src',src);
- $('head').append(s);
-}
-_chm_as ("http://bioinformatics.mdanderson.org/chmv/js/libs/dynamic_viewer.v2.js");
-chm.addCustomization(function(){
-    var dvapp = new DynamicViewer.DVApp ("/DynamicViewer", "dynjs", "getchmcustom");
-    var params = { 'chm': chm.mapName };
-    var target_id = "noonewantsme";
-    var target = $('<div></div>', { id: target_id, class: 'offscreen' });
-    $('body').append(target);
-    DynamicViewer.invoke (dvapp, params, target_id, function (error, result) {
-	if (error) {
-            _chm_as (chm.urlBase + "/undefined/custom-backup.js");
-	} else {
-            _chm_as (result);
-	}
+    $.getScript ("/resources/dyce.js", function() {
+	chm.addCustomization(function(){
+	    var dvapp = new DynamicViewer.DVApp ("/DynamicViewer", "dynjs", "getchmcustom");
+	    var params = { 'chm': chm.mapName };
+	    var target_id = "noonewantsme";
+	    var target = $('<div></div>', { id: target_id, class: 'offscreen' });
+	    $('body').append(target);
+	    DynamicViewer.invoke (dvapp, params, target_id, function (error, result) {
+		$.getScript (error ? chm.urlBase + "/undefined/custom-backup.js" : result);
+	    });
+	});
     });
-});
 })(MDACC_GLOBAL_NAMESPACE.namespace('tcga').chm);
