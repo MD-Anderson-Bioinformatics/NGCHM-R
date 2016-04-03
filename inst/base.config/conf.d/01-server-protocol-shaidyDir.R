4e6fbee2b4479ae9dfe2959ceefdd9ab71698d40
Bradley Broom <bmbroom@mdanderson.org>
2016-04-03 11:15:15 -0500

    Attempt to tile data only if tiledata command is available

--- inst/base.config/conf.d/01-server-protocol-shaidyDir.R
+++ inst/base.config/conf.d/01-server-protocol-shaidyDir.R
@@ -26,7 +26,7 @@ ngchmCreateServerProtocol ("shaidydir",
             stopifnot (length(repo) > 0);
             shaidyCopyBlob (repo, sid, shaidyRepo);
         }
-        if (TRUE) {
+        if (testExternalProgram('tiledata')) {
             tiles <- mapply(function(x) {
                 ngchmTileDataset(shaidyRepo,x@data,chm@rowOrder,chm@colOrder)[[1]]
             }, chm@layers, SIMPLIFY=FALSE);
