babef2729b4e67d4b0622b9add2bbc63b3c933f7
marohrdanz <marohrdanz@mdanderson.org>
2023-11-22 09:43:31 -0600

    Run styler::style_file() on test-gitSha.R

--- tests/testthat/test-gitSha.R
+++ tests/testthat/test-gitSha.R
@@ -1,8 +1,8 @@
-library (NGCHM)
+library(NGCHM)
 context("git digest")
 
 test_that("gitSha matches git sha computation", {
-    # Test cases from http://stackoverflow.com/questions/552659/assigning-git-sha1s-without-git
-    expect_equal (gitSha(""), "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391")
-    expect_equal (gitSha("foobar\n"), "323fae03f4606ea9991df8befbb2fca795e648fa")
+  # Test cases from http://stackoverflow.com/questions/552659/assigning-git-sha1s-without-git
+  expect_equal(gitSha(""), "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391")
+  expect_equal(gitSha("foobar\n"), "323fae03f4606ea9991df8befbb2fca795e648fa")
 })
